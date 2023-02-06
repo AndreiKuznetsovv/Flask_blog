ada3c45947c845c838b1c3aa954a2e56b2263258
Andrei <yeloww51@gmail.com>
2023-02-06 09:30:59 +0300

    message 2

--- website/views.py
+++ website/views.py
@@ -126,7 +126,7 @@ def user_posts(username: str):
 def create_comment(post_id: int):
     if request.method == 'POST':
         text = request.form.get('text')
-
+        # comment 123
         if not text:
             flash('Comment cannot be empty', category='error')
             return redirect(url_for('.home'))
